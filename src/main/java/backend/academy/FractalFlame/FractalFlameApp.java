0851f06073478ed8facaa085a7eea76b262f0e9b
Excellent <7074172@gmail.com>
2024-11-24 15:26:22 +0300

    Adding fractal images, assembling likes, calculating performance

--- src/main/java/backend/academy/FractalFlame/FractalFlameApp.java
+++ src/main/java/backend/academy/FractalFlame/FractalFlameApp.java
@@ -16,6 +16,7 @@ import backend.academy.FractalFlame.transformations.ColorTransformation;
 import backend.academy.FractalFlame.transformations.LinearTransformation;
 import backend.academy.FractalFlame.utils.ImageUtils;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -33,9 +34,10 @@ import org.apache.logging.log4j.Logger;
  * @since 1.0
  */
 public class FractalFlameApp {
-    private static final Logger LOGGER = LogManager.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger(FractalFlameApp.class);
 
     private FractalFlameApp() {
+        throw new UnsupportedOperationException("Utility class");
     }
 
     /**
@@ -45,67 +47,101 @@ public class FractalFlameApp {
      *            конфигурация для генерации фрактала
      */
     public static void generateAndSaveFractal(Config config) {
-        List<ColorTransformation> affineTransformations = prepareAffineTransformations(
-                config.getRandomAffineTransformationsCount(), config.getPresetAffineTransformations());
-
-        Renderer renderer = config.getThreadsCount() <= 1 ? new SingleRenderer(config.getSymmetry())
-                : new ParallelRenderer(config.getThreadsCount(), config.getSymmetry());
-
-        IFractalImage imageBase = config.getThreadsCount() <= 1
-                ? FractalImage.create(config.getWidth(), config.getHeight())
-                : SyncFractalImage.create(config.getWidth(), config.getHeight());
-
-        IFractalImage fractalImage = renderer.render(imageBase,
-                new Rectangular(config.getMinX(), config.getMinY(), config.getMaxX() - config.getMinX(),
-                        config.getMaxY() - config.getMinY()),
-                affineTransformations, Arrays.stream(config.getNonlinearTransformations()).toList(),
-                config.getSamples(), config.getIterations(), config.getSeed());
-
-        if (config.isWithCorrection()) {
-            ImageProcessor processor = new GammaCorrectionProcessor(config.getGamma());
-            processor.process(fractalImage);
-        }
-
         try {
-            ImageUtils.save(fractalImage,
-                    Path.of(config.getDirectory(),
-                            config.getFilename() + "." + config.getFileType().toString().toLowerCase()),
-                    config.getFileType());
-        } catch (IOException exception) {
-            LOGGER.error("Failed to save fractal image", exception);
+            // Подготовка трансформаций
+            List<ColorTransformation> affineTransformations = prepareAffineTransformations(
+                    config.getRandomAffineTransformationsCount(), config.getPresetAffineTransformations());
+
+            // Выбор рендерера
+            Renderer renderer = createRenderer(config);
+
+            // Создание холста
+            IFractalImage canvas = createCanvas(config);
+
+            // Рендеринг
+            IFractalImage fractalImage = renderer.render(canvas, createWorldRect(config), affineTransformations,
+                    Arrays.asList(config.getNonlinearTransformations()), config.getSamples(), config.getIterations(),
+                    config.getSeed());
+
+            // Коррекция изображения, если указано
+            if (config.isWithCorrection()) {
+                applyGammaCorrection(fractalImage, config.getGamma());
+            }
+
+            // Сохранение изображения
+            saveFractalImage(fractalImage, config);
+        } catch (Exception e) {
+            LOGGER.error("Error during fractal generation and saving", e);
         }
     }
 
     /**
+     * Создаёт рендерер на основе конфигурации.
+     */
+    private static Renderer createRenderer(Config config) {
+        return config.getThreadsCount() <= 1 ? new SingleRenderer(config.getSymmetry())
+                : new ParallelRenderer(config.getThreadsCount(), config.getSymmetry());
+    }
+
+    /**
+     * Создаёт холст для рендеринга.
+     */
+    private static IFractalImage createCanvas(Config config) {
+        return config.getThreadsCount() <= 1 ? FractalImage.create(config.getWidth(), config.getHeight())
+                : SyncFractalImage.create(config.getWidth(), config.getHeight());
+    }
+
+    /**
+     * Создаёт прямоугольник мира для рендеринга.
+     */
+    private static Rectangular createWorldRect(Config config) {
+        return new Rectangular(config.getMinX(), config.getMinY(), config.getMaxX() - config.getMinX(),
+                config.getMaxY() - config.getMinY());
+    }
+
+    /**
+     * Применяет гамма-коррекцию к изображению.
+     */
+    private static void applyGammaCorrection(IFractalImage image, double gamma) {
+        ImageProcessor processor = new GammaCorrectionProcessor(gamma);
+        processor.process(image);
+    }
+
+    /**
+     * Сохраняет сгенерированное фрактальное изображение.
+     */
+    private static void saveFractalImage(IFractalImage image, Config config) throws IOException {
+        Path outputDir = Path.of(config.getDirectory());
+        if (!Files.exists(outputDir)) {
+            Files.createDirectories(outputDir);
+        }
+
+        Path outputPath = outputDir.resolve(config.getFilename() + "." + config.getFileType().toString().toLowerCase());
+
+        ImageUtils.save(image, outputPath, config.getFileType());
+        LOGGER.info("Fractal image saved to: {}", outputPath);
+    }
+
+    /**
      * Подготавливает список аффинных преобразований.
-     *
-     * @param randomCount
-     *            количество случайных преобразований
-     * @param preset
-     *            массив предустановленных преобразований
-     *
-     * @return список цветовых аффинных преобразований
      */
     private static List<ColorTransformation> prepareAffineTransformations(int randomCount,
             AffineTransformation[] preset) {
-        List<ColorTransformation> affineTransformations = new ArrayList<>();
+        List<ColorTransformation> transformations = new ArrayList<>();
 
         // Генерация случайных аффинных преобразований
         for (int i = 0; i < randomCount; i++) {
-            affineTransformations
-                    .add(new ColorTransformation(LinearTransformation.randomTransformation(), Color.generate()));
+            transformations.add(new ColorTransformation(LinearTransformation.randomTransformation(), Color.generate()));
         }
 
-        // Добавление preset преобразований
+        // Добавление предустановленных преобразований
         if (preset != null) {
-            for (AffineTransformation presetObj : preset) {
-                affineTransformations.add(new ColorTransformation(
-                        new LinearTransformation(presetObj.a(), presetObj.b(), presetObj.c(), presetObj.d(),
-                                presetObj.e(), presetObj.f()),
-                        new Color(presetObj.red(), presetObj.green(), presetObj.blue())));
+            for (AffineTransformation affine : preset) {
+                transformations.add(new ColorTransformation(new LinearTransformation(affine.a(), affine.b(), affine.c(),
+                        affine.d(), affine.e(), affine.f()), new Color(affine.red(), affine.green(), affine.blue())));
             }
         }
 
-        return affineTransformations;
+        return transformations;
     }
 }
